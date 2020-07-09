3f2bec98bd89090001dd606023fadce324b274d4
Lily <misswwswimt@gmail.com>
2020-07-09 16:18:39 +0600

    rendered airhumidity data

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,7 +6,7 @@ import './nav.css'
 const navItems = [
   { id: 1, link: '/', text: 'Dust' },
   { id: 2, link: '/temperature', text: 'Temperature' },
-  { id: 3, link: '/air-humidity', text: 'Air humidity' },
+  { id: 3, link: '/air_humidity', text: 'Air humidity' },
   { id: 4, link: '/subscribe', text: 'Subscribe' },
   { id: 5, link: '/statistics', text: 'Statistics' },
   // { id: 4, link: '/contacts', text: 'Contacts' },
