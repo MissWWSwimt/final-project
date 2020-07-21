1f5efe40971c9ac1629e1adbeb1a7b0c3812a948
Lily <misswwswimt@gmail.com>
2020-07-21 13:24:17 +0600

    Updates

---
+++ precache-manifest.c2fdfee2f8034fdeef6829b67644e7f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdfba36e3d0c2e567a8a413eb901c32",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "da2bf897c05720a9fffd",
+    "url": "/final-project/static/css/main.16a93bc1.chunk.css"
+  },
+  {
+    "revision": "26b66adc289b8f1a83c1",
+    "url": "/final-project/static/js/2.d03151f3.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/final-project/static/js/2.d03151f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2bf897c05720a9fffd",
+    "url": "/final-project/static/js/main.4d2b79ac.chunk.js"
+  },
+  {
+    "revision": "b727c98e90d9646c605e",
+    "url": "/final-project/static/js/runtime-main.45b6dddd.js"
+  },
+  {
+    "revision": "7277480fe84d8ff18330681948ea7f0b",
+    "url": "/final-project/static/media/marker.7277480f.svg"
+  }
+]);
